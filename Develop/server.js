65c011a860f736abdc3cd04ba3e1732fa6e6e09c
Yazid <yalhujery01@gmail.com>
2022-03-07 09:20:32 -0500

    .

---
+++ Develop/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const PORT = 3001;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(
+   
+  );
+});
+
+app.get('11-Portfolio\11-Portfolio\Develop\public\notes.html', (req, res) => {
+  res.json({
+  });
+});
